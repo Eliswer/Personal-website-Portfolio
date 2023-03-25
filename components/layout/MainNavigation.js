52175dc6c58da5db7e65693dd4d695c994e7ec07
Eliswer <106431426+Eliswer@users.noreply.github.com>
2023-03-25 20:39:30 +0100

    navigation ++

--- components/layout/MainNavigation.js
+++ components/layout/MainNavigation.js
@@ -1,13 +1,19 @@
-import React from "react";
+import classes from "./mainNavigation.module.css";
+
+import Image from "next/image";
+import codeIcon from "../../public/icons/codeIcon.png";
 
 function mainNavigation() {
   return (
-    <div className="wrapper">
-      <ul className="links">
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
+    <div className={classes.wrapper}>
+      <div className={classes.codeIcon}>
+        <Image src={codeIcon} alt="htmlTagIcon" />
+      </div>
+      <ul className={classes.links}>
+        <li>01 About me</li>
+        <li>02 Skills</li>
+        <li>03 Projects</li>
+        <li>04 Contact me</li>
         <button>Resume</button>
       </ul>
     </div>
