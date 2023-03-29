ab40a64dc88392be43bd46f77162c3cb772a66f9
Eliswer <106431426+Eliswer@users.noreply.github.com>
2023-03-29 15:02:50 +0200

    02 skills init

--- components/Skills.js
+++ components/Skills.js
@@ -1,9 +1,50 @@
 import Wrapper from "./layout/Wrapper";
+import classes from "./skills.module.css";
+
+const languagesUrls = [
+  { alt: "Html logo", url: "" },
+  { alt: "Css logo", url: "" },
+  { alt: "Sass logo", url: "" },
+  { alt: "JS logo", url: "" },
+];
+
+const backendTechnologiesUrls = [
+  { alt: "Node JS logo", url: "" },
+  { alt: "Express logo", url: "" },
+  { alt: "EJS logo", url: "" },
+  { alt: "Rest API logo", url: "" },
+];
+
+const otherUrls = [
+  { alt: "Git logo", url: "" },
+  { alt: "Docker logo", url: "" },
+  { alt: "Command line logo", url: "" },
+  { alt: "Redux Toolkit logo", url: "" },
+  { alt: "NPM logo", url: "" },
+];
+
+const librariesFrameworksUrls = [
+  { alt: "Styled Components logo", url: "" },
+  { alt: "Tailwind Css logo", url: "" },
+  { alt: "jQuery logo", url: "" },
+  { alt: "React logo", url: "" },
+];
+
+const softwareUrls = [
+  { alt: "VScode logo", url: "" },
+  { alt: "PHPstorm logo", url: "" },
+  { alt: "Postman logo", url: "" },
+  { alt: "Trello logo", url: "" },
+  { alt: "Github Desktop logo", url: "" },
+  { alt: "Adobe After Effects logo", url: "" },
+  { alt: "Sony Vegas pro", url: "" },
+  { alt: "Figma logo", url: "" },
+];
 
 function Skills(props) {
   return (
     <Wrapper>
-      <div>Skills</div>
+      <div className={classes.wrapper}>Skills</div>
     </Wrapper>
   );
 }
---
+++ components/Technologies.js
@@ -0,0 +1,7 @@
+import classes from "./technologies.module.css";
+
+function Skills({ title }) {
+  return <div></div>;
+}
+
+export default Skills;
