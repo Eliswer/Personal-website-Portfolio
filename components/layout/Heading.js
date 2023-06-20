0b770782bef785c238d2ad6a1873539e04eb4d3c
Eliswer <106431426+Eliswer@users.noreply.github.com>
2023-06-20 10:49:41 +0200

    deleted styles that didnt work

--- components/layout/Heading.js
+++ components/layout/Heading.js
@@ -1,33 +1,9 @@
-import { useInView } from "react-intersection-observer";
 import classes from "./heading.module.css";
 import Image from "next/image";
 
 import Triangle from "../../public/imgs/PurpleTriangleHeading.png";
-import { useEffect } from "react";
 
 function Heading({ number, title }) {
-  const { ref: myRef, inView: myElementIsVisible } = useInView();
-
-  let newStyles = {
-    color: "black",
-    fontFamily: "monospace",
-    overflow: "hidden",
-    borderRight: ".15em solid orange",
-    whiteSpace: "nowrap",
-    margin: "0 auto",
-  };
-  let newStyling;
-
-  useEffect(() => {
-    if (myElementIsVisible) {
-      newStyling = newStyles;
-    } else {
-      newStyling = { visibility: "visibile" };
-    }
-  }, [myElementIsVisible]);
-
-  console.log(myElementIsVisible);
-
   return (
     <div className={classes.wrapper} style={{ position: "relative" }}>
       <div className={classes.triangleWrapper}>
@@ -35,7 +11,7 @@ function Heading({ number, title }) {
       </div>
       <h2 className={`${classes.title} gradient-text`}>
         <span className={classes["title-number"]}>{number}</span>
-        <span ref={myRef} className={classes["title-text"]} style={newStyling}>
+        <span ref={myRef} className={classes["title-text"]}>
           {title}
         </span>
       </h2>
